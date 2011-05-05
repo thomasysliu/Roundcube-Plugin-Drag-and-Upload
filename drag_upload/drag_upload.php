c8655f194d03fc2c0ba4161a36f7bf9ff5033ed3
ysliu <nckuhuahua@gmail.com>
2011-05-05 21:33:45 +0800

    Add main php file and Update README

---
+++ drag_upload/drag_upload.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Drag Upload
+ *
+ * A plugin to utilize html5 file API to upload attachments
+ *
+ * @version 1.0
+ * @author Thomas Yu - Sian , Liu
+ * @url https://github.com/thomasysliu/Roundcube-Plugin-Drag-and-Upload
+ */
+class drag_upload extends rcube_plugin
+{
+  public $task = 'mail';
+
+  function init()
+  {
+
+  }
+
+}
+
+?>
